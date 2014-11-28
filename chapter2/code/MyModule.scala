5d84411101554f0bb50cd99045a8c56b6a59ac27
vijaykiran <mail@vijaykiran.com>
2014-11-28 01:01:56 +0100

    Fib

--- chapter2/code/MyModule.scala
+++ chapter2/code/MyModule.scala
@@ -12,6 +12,26 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <=0 ) acc
+      else go(n-1, n*acc)
+
+    go(n, 1)
+  }
+
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(pos: Int, cur: Int, next: Int): Int =
+      if (pos == n) cur
+      else go(pos + 1, next, cur + next)
+
+    go(0, 0, 1)
+
+  }
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
 }
